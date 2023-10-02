e8caf7e72d24ea2c10348122756069fc7ca2339e
Nadar <donat290817@gmail.com>
2023-10-02 23:13:37 +0300

    Интеграция WeatherApi в приложение

---
+++ src/main/java/com/example/weatherreport_perproject/Services/WeatherApiService.java
@@ -0,0 +1,19 @@
+package com.example.weatherreport_perproject.Services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherApiService {
+    private final String API_KEY = "b91e5bc13e00446abfa193112230210";
+    private final String API_URL = "http://api.weatherapi.com/v1/current.json?q={city}&key={apiKey}";
+    private final RestTemplate restTemplate;
+    public WeatherApiService(RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
+    }
+    public String getWeatherByCity(String city){
+        String apiUrl = API_URL.replace("{city}", city).replace("{apiKey}", API_KEY);
+        String response = restTemplate.getForObject(apiUrl, String.class);
+        return response;
+    }
+}
--- src/main/java/com/example/weatherreport_perproject/WeatherReportPerProjectApplication.java
+++ src/main/java/com/example/weatherreport_perproject/WeatherReportPerProjectApplication.java
@@ -2,6 +2,10 @@ package com.example.weatherreport_perproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.beans.BeanProperty;
 
 @SpringBootApplication
 public class WeatherReportPerProjectApplication {
@@ -9,5 +13,8 @@ public class WeatherReportPerProjectApplication {
     public static void main(String[] args){
         SpringApplication.run(WeatherReportPerProjectApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
